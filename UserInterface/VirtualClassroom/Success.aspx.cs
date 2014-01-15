8590579c3da069c041adb5124616fe29cc99cc68
Akrutipatel <akritiptl@yahoo.com>
2014-01-15 11:11:22 +0530

    VideoUpload.cs file added

--- UserInterface/VirtualClassroom/Success.aspx.cs
+++ UserInterface/VirtualClassroom/Success.aspx.cs
@@ -4,19 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using CMS.Logic.VirtualClassroom;
+using CMS.Logic;
 
 namespace CMS.UserInterface.VirtualClassroom
 {
     public partial class Success : System.Web.UI.Page
     {
+
+        VideoUpload vup = new VideoUpload();
+        Functions func = new Functions();
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Page.Request.QueryString["status"] != null)
             {
+                
                 String status = Page.Request.QueryString[0];
+                String title = Session["VideoTitle"].ToString();
+                String description = Session["VideoDescription"].ToString();
+                String key=Page.Request.QueryString["id"];
+                String staffid = func.getStaffId(Session["UserID"].ToString());
+                
                 int statusCode = Int32.Parse(status);
                 if (statusCode == 200)
                 {
+                    vup.addInVideoMaster(title,key,description,staffid);
                     status_text.Text = "Upload Successful with status : " + statusCode;
                     video_key.Text = "Video Key : " + Page.Request.QueryString["id"];
                     info.Text = "Please be patient as youtube processes the video. It will be available after 1 min.";
--- UserInterface/VirtualClassroom/VideoUpload.aspx.cs
+++ UserInterface/VirtualClassroom/VideoUpload.aspx.cs
@@ -2,31 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using Google.GData.Client;
-using Google.GData.Extensions;
+using CMS.Logic.VirtualClassroom;
 using Google.GData.YouTube;
-using Google.GData.Extensions.MediaRss;
-using Google.YouTube;
-using Google.GData.Extensions.Location;
-using System.Net;
-using System.Web.Services;
 
 namespace CMS.UserInterface.StaffPortal
 {
     public partial class TestVideos : System.Web.UI.Page
     {
-        Video newVideo = new Video();
-        public string urlName = "";
-        public string tokenValue = "";
 
+        public VideoUpload vup = new VideoUpload();
+        public String urlName = "";
+        public String tokenValue = "";
+        
         protected void Page_Load(object sender, EventArgs e)
         {
-            if(Page.IsPostBack)
-            {
-                this.add_Video(sender, e);
-            }
+  
         }
 
         
@@ -39,53 +29,13 @@ namespace CMS.UserInterface.StaffPortal
                 videoUp.Attributes.Remove("style");
                 videoUp.Attributes.Add("style", "display:block");
             }
-            String developerKey = "AI39si4BpxpB81zKCC2Ofo1MbC6CjC7Z_S_gaZ_Fz2ouQXj5n55freXGXhJMxiM4kDXt2IF90Ho0gKox_AytvMGLjAw0FsAq9A";
-            String username = "gecgsec28@gmail.com";
-            String pwd = "cmsgecg28";
-            YouTubeRequestSettings settings = new YouTubeRequestSettings("cms_app", developerKey, username, pwd);
-            YouTubeRequest request = new YouTubeRequest(settings);
-            Video newVideo = new Video();
-            newVideo.Title = Text_title.Text;
-            newVideo.Tags.Add(new MediaCategory("Education", YouTubeNameTable.CategorySchema));
-            newVideo.Keywords = Text_keyword.Text;
-            newVideo.Description = Text_description.Text;
-            newVideo.YouTubeEntry.Private = false;
-            newVideo.YouTubeEntry.Location = new GeoRssWhere(37, -122);
-            newVideo.Tags.Add(new MediaCategory("CMS, GECG28", YouTubeNameTable.DeveloperTagSchema));
-            FormUploadToken token = request.CreateFormUploadToken(newVideo);
-            urlName = token.Url;
-            tokenValue = token.Token;
-            //Response.Write("URL:" + urlName + "\n");
-            //Response.Write("Token:" + tokenValue + "\n");
+            Session["VideoTitle"] = Text_title.Text;
+            Session["VideoDescription"] = Text_description.Text;
+            Session["VideoKeyword"] = Text_keyword.Text;
+            FormUploadToken formtoken = vup.addMetadata(Text_title.Text, Text_description.Text, Text_keyword.Text);
+            urlName = formtoken.Url;
+            tokenValue = formtoken.Token;
         }
 
-
-
-        protected void button_click(object sender, EventArgs e)
-        {
-            if (newVideo.IsDraft)
-            {
-                Console.WriteLine("Video is not live.");
-                string stateName = newVideo.Status.Name;
-                if (stateName == "processing")
-                {
-                    Console.WriteLine("Video is still being processed.");
-                }
-                else if (stateName == "rejected")
-                {
-                    Console.Write("Video has been rejected because: ");
-                    Console.WriteLine(newVideo.Status.Value);
-                    Console.Write("For help visit: ");
-                    Console.WriteLine(newVideo.Status.Help);
-                }
-                else if (stateName == "failed")
-                {
-                    Console.Write("Video failed uploading because:");
-                    Console.WriteLine(newVideo.Status.Value);
-                    Console.Write("For help visit: ");
-                    Console.WriteLine(newVideo.Status.Help);
-                }
-            }
-        }
     }
 }
